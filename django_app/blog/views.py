92dab1c6bb37b3b1136b407adabf3d2ad6456e08
junho Kwon <oilvers0904@gmail.com>
2017-05-29 16:47:23 +0900

    complete djangogirls tutorial

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
     # return HttpResponse('<html><body>Post List</body</html>')
-    return render(request, 'blog/post_list.html')
+    # posts = Post.objects.all()
+    # print(posts)
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    context = {
+        'title' : "PostList from post_list view",
+        'posts' : posts,
+    }
+    return render(request, 'blog/post_list.html',context=context)
